1bd0c88abc1317cd6b08b8027459ce96df39ad03
hamzafallahi <hamzafallahi7@gmail.com>
2024-11-21 14:27:27 +0100

    release version 8.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**", // matches any path
-      },
-      {
-        protocol: "https",
-        hostname: "assets.aceternity.com",
-        pathname: "/**", // matches any path
-      },
-    ],
-    //unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //  ignoreDuringBuilds: true,
+  //},
 };
 
 export default nextConfig;
